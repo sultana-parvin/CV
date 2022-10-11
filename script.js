5196f5af9dad51cc12e3ef476115ba2699828321
unknown <sultana.onlineformapro@gmail.com>
2022-10-11 12:07:29 +0200

    new code

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contacts').addEventListener('click', function(){
+    const emailLink = contacts.innerText
+    contacts.innerText = 'sultana.onlineformapro@gmail.com';
+    
+})
